aefba9522f31caff4767e1a66547c0cc99ccdf95
Vivek-k001 <vk3699010@gmail.com>
2025-01-03 22:35:31 +0530

    trim

---
+++ python/trim space.py
@@ -0,0 +1,8 @@
+
+given_string = "G  o o d    M o r n    in g "
+
+print("Before removing white spaces given string=", given_string)
+
+given_string = given_string.replace(" ", "")
+
+print("after removing white spaces given string=", given_string)
