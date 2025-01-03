872731b324ad0a06a71de59c0c4d462d21cd6d58
Vivek-k001 <vk3699010@gmail.com>
2025-01-03 22:33:04 +0530

    reverse

---
+++ python/multiplication.py
@@ -0,0 +1,3 @@
+number = 8
+for i in range(1, 11):
+    print(number, "*", i, "=", number*i)
---
+++ python/reverse string.py
@@ -0,0 +1,7 @@
+given_string = "ABCDRFG"
+
+length = len(given_string)
+
+reverse_string = given_string[len(given_string)::-1]
+
+print("The reversed string of", given_string, "=", reverse_string)
